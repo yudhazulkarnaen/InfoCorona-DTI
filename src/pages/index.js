9cee2050d517aaa74a5858d05c1d89c372284906
Muhamad Zulkarnaen Yudha Bahari <36037631+yudhazulkarnaen@users.noreply.github.com>
2020-12-12 15:37:52 +0700

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export { default as Contact } from './contact';
 export { default as Login } from './login';
 export { default as ProfileId } from './profileId';
 export { default as InfoCorona } from './infoCorona';
+export { default as DetailDate } from './detailDate';
+export { default as Product } from './product';
