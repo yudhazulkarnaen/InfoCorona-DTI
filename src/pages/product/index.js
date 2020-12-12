14d6e9960bb401be5094180308a6d05705849bed
Muhamad Zulkarnaen Yudha Bahari <36037631+yudhazulkarnaen@users.noreply.github.com>
2020-12-12 15:50:19 +0700

    Add files via upload

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,86 +1,123 @@
-import React, { useState } from 'react';
-import './style.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import swal from 'sweetalert';
-import { setCookie } from '../../utils/cookie';
+import React, { useState, useEffect } from 'react';
+import { CommonLoading } from 'react-loadingg';
+import { TablePagination } from '@material-ui/core';
 import { authService } from '../../services';
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css';
 
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Product = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [dataProduct, setDataProduct] = useState([]);
+  const [keyword, setKeyword] = useState('');
+  // const limit = 9;
+  // const offset = 0;
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowPerPage] = useState(9);
+  useEffect(() => {
+    const getProduct = () => {
+      authService
+        .getProduct(keyword, rowsPerPage, page)
+        .then((res) => {
+          // eslint-disable-next-line no-console
+          console.log(res);
+          setDataProduct(res.data);
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.log(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+    getProduct();
+  }, [keyword, page, rowsPerPage]);
 
-  const [isLoginLoading, setLoginLoading] = useState(false);
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-  const onSubmitLogin = () => {
-    setLoginLoading(true);
-    authService
-      .login(username, password)
-      .then((res) => {
-        // eslint-disable-next-line no-console
-        console.log(res);
-        const cookieToken = res.token;
-        const cookieUser = {
-          userId: res.userId,
-          username: res.username,
-        };
-        setCookie('userData', JSON.stringify(cookieUser), 10000);
-        setCookie('token', JSON.stringify(cookieToken), 10000);
-        window.location.replace('/product');
-      })
-      .catch((err) => {
-        swal('Password Salah!', err.data.message, 'error');
-        // eslint-disable-next-line no-console
-        console.log(err);
-      })
-      .finally(() => {
-        setLoginLoading(false);
-      });
+  const handleChangePage = (event, newPage) => {
+    scrollUp();
+    setPage(newPage);
+    // getProduct();
+  };
+
+  const handleChangeRowPerPage = (event) => {
+    setRowPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+    // getProduct();
   };
 
   return (
     <div>
-      <div className="login-clean">
+      {isLoading ? (
+        <CommonLoading />
+      ) : (
+        <center>
+          <h1>Data Product</h1>
+        </center>
+      )}
+      <div className="container">
         <form
+          className="example"
           onSubmit={(e) => {
             e.preventDefault();
-            onSubmitLogin();
+            // eslint-disable-next-line no-undef
+            // getProduct();
           }}
         >
-          <h2>Login Form</h2>
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              className="form-control"
-              placeholder="Password"
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              className="btn btn-primary btn-block"
-              type="submit"
-              value="Login"
-              disabled={isLoginLoading}
-            />
-          </div>
+          <input
+            type="text"
+            placeholder="Search.."
+            value={keyword}
+            onChange={(e) => {
+              setKeyword(e.target.value);
+            }}
+          />
+          <input
+            className="btn-xs btn-primary"
+            type="submit"
+            value="Cari"
+            disable
+          />
         </form>
+        <div className="row">
+          {dataProduct.map((product) => {
+            return (
+              <div className="col-sm-4">
+                <div className="card border-dark mb-3">
+                  <div className="card-header">{product.name}</div>
+                  <div className="card-body text-dark">
+                    <h5 className="card-title">Harga</h5>
+                    <p className="card-text">
+                      {product.display_promo_price_percentage}
+                      {'  '}
+                      {product.display_normal_price}
+                    </p>
+                    <h3>{product.display_price}</h3>
+                    <h6>{product.description}</h6>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <TablePagination
+            rowsPerPageOptions={[3, 9, 25]}
+            component="div"
+            count={100}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowPerPage}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Product;
